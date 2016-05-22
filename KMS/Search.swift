252ba4295fb22826d9ca76d0a5d6610a204a34da
John <jphouerou@gmail.com>
2016-05-22 19:04:23 +0200

    Changed profile pictures to categories icons and fixed UIController bug

---
+++ KMS/Search.swift
@@ -0,0 +1,32 @@
+//
+//  Search.swift
+//  KMS
+//
+//  Created by John Le Houerou on 05/05/2016.
+//  Copyright © 2016 John Le Houerou. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    
+    @IBOutlet var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //let url = NSURL (string: "https://m.facebook.com/");
+        //let requestObj = NSURLRequest(URL: url!);
+        //webview.loadRequest(requestObj);
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
