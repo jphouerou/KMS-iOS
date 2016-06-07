bc585312124491c99a01a9277357bad217c3e0af
John <John@Johns-MacBook-Pro.local>
2016-06-07 13:59:42 +0200

    Homepage PHP

---
+++ PHP/home.php
@@ -0,0 +1,39 @@
+<?php
+ 
+// Create connection
+$con=mysqli_connect("localhost","root","","kms");
+ 
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+ 
+// This SQL statement selects ALL from the table 'Locations'
+//$sql = "SELECT * FROM post WHERE postCategory = 'IT' LIMIT 50";
+$sql = "SELECT * FROM post, user WHERE user.email = 'snow@email.com' AND user.department = post.postCategory LIMIT 50";
+
+ 
+// Check if there are results
+if ($result = mysqli_query($con, $sql))
+{
+	// If so, then create a results array and a temporary one
+	// to hold the data
+	$resultArray = array();
+	$tempArray = array();
+ 
+	// Loop through each row in the result set
+	while($row = $result->fetch_object())
+	{
+		// Add each row into our results array
+		$tempArray = $row;
+	    array_push($resultArray, $tempArray);
+	}
+ 
+	// Finally, encode the array to JSON and output the results
+	echo json_encode($resultArray);
+}
+ 
+// Close connections
+mysqli_close($con);
+?>
